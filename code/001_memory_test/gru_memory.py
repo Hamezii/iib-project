f579f5e82e0206ad0112450c758523b2a9f9673c
Hamezii <hameziipost@gmail.com>
2025-01-07 15:47:16 +0000

    Add training over a range of sequence lengths

--- code/001_memory_test/gru_memory.py
+++ code/001_memory_test/gru_memory.py
@@ -20,16 +20,20 @@ NUM_CLASSES = 10
 INPUT_SIZE = NUM_CLASSES + 1
 HIDDEN_SIZE = 32
 
-LEARNING_STEPS = 3000
-LEARNING_RATE = 0.04
+LEARNING_STEPS = 4000
+LEARNING_RATE = 0.05
 
 
 # ---- Task data
-BATCH_SIZE = 64
+BATCH_SIZE = 32
 SEQ_LENGTH = 8
-INPUT_STEP_T = 1  # TODO Implement
-NOISE_STD = 0.05
+INPUT_STEP_T = 1 # tensor.repeat_interleave(N) repeats values in a tensor
+T_AFTER_GO_DELAY = 0
+T_SEQUENCE = SEQ_LENGTH * INPUT_STEP_T
+T_FULL_INPUT = (SEQ_LENGTH+1) * INPUT_STEP_T
+T_OUTPUT_START = T_SEQUENCE + T_AFTER_GO_DELAY
 
+NOISE_STD = 0.05
 
 class RandomSequenceGenerator:
     """Generates random input sequences"""
@@ -44,10 +48,13 @@ class RandomSequenceGenerator:
 
         # Getting one-hot sequence from index tensor
         # pylint: disable=E1102
-        x = F.one_hot(x_indices, num_classes=NUM_CLASSES+1).float().to(device)
+        x = F.one_hot(x_indices, num_classes=NUM_CLASSES+1) \
+            .float() \
+            .repeat_interleave(INPUT_STEP_T, dim=0) \
+            .to(device)
 
         assert seq_indices.shape == (SEQ_LENGTH,)
-        assert x.shape == (SEQ_LENGTH + 1, NUM_CLASSES+1)
+        assert x.shape == (T_FULL_INPUT, NUM_CLASSES+1), x.shape
         return x, seq_indices
 
 # pylint: disable=W0223
@@ -140,6 +147,9 @@ class MemoryModel(nn.Module):
         nn.init.xavier_uniform_(self.out_layer.weight)
         nn.init.zeros_(self.out_layer.bias)
 
+        # Pooling ys for each input element
+        self.pool = nn.AvgPool1d(kernel_size=INPUT_STEP_T)
+
     def forward(self, x):
         """Runs the GRU cell once for each step."""
         # assert x.shape == (BATCH_SIZE, SEQ_LENGTH+1, NUM_CLASSES+1)
@@ -148,57 +158,89 @@ class MemoryModel(nn.Module):
         h_t = torch.zeros(x.shape[0], HIDDEN_SIZE).to(device)
 
         # Initial y matrix
-        y = torch.zeros((x.shape[0], SEQ_LENGTH, NUM_CLASSES)).to(device)
+        y = torch.zeros((x.shape[0], T_SEQUENCE, NUM_CLASSES)).to(device)
 
         # Zero matrix for no input
         zero_input = torch.zeros((x.shape[0], INPUT_SIZE)).to(device)
 
         # Iteratively modelling
-        for i in range(SEQ_LENGTH * 2):
+        for i in range(T_OUTPUT_START + T_SEQUENCE):
             # Could experiment with replacing zero_input with prev:
             # prev = torch.cat((y[:, i - SEQ_LENGTH - 1, :], torch.zeros((x.shape[0], 1)).to(device)), dim=1).to(device)
             x_slice = x[:, i, :] if i < x.shape[1] else zero_input
             # Noise
             noise = NOISE_STD * torch.randn(x_slice.shape).to(device)
             h_t = self.grucell(x_slice+noise, h_t)
-            if i >= SEQ_LENGTH:
-                y[:, i - SEQ_LENGTH, :] = self.out_layer(h_t)
+            if i >= T_OUTPUT_START:
+                y[:, i - T_OUTPUT_START, :] = self.out_layer(h_t)
 
+        y = y.transpose(1, 2)
+        y = self.pool(y) # Averaging across individual inputs
         return y
 
 
 dataset = SequenceDataset()
 loader = DataLoader(dataset, batch_size=BATCH_SIZE)
-model = MemoryModel(MinGRUCell).to(device)
-loss_func = nn.CrossEntropyLoss(ignore_index=INPUT_SIZE-1)
+model = MemoryModel(GRUCell).to(device)
+loss_func = nn.CrossEntropyLoss(ignore_index=INPUT_SIZE-1).to(device)
 optimizer = optim.Adam(model.parameters(), lr=LEARNING_RATE)
 scheduler = optim.lr_scheduler.ReduceLROnPlateau(
-    optimizer, mode='min', factor=0.25, patience=2)
+    optimizer, mode='min', factor=0.25, patience=10)
+
+seq_length_min = 2
+seq_length_max = 5
+SEQ_LENGTH = seq_length_min
+T_SEQUENCE = SEQ_LENGTH * INPUT_STEP_T
+T_FULL_INPUT = (SEQ_LENGTH+1) * INPUT_STEP_T
+T_OUTPUT_START = T_SEQUENCE + T_AFTER_GO_DELAY
+
 LR_UPDATES = 50
 i = 0
 avg_loss = 0
+learn_loss = 0
+
+
+model.train()
 for (x, seq_indices) in loader:
-    assert x.shape == (BATCH_SIZE, SEQ_LENGTH + 1, NUM_CLASSES+1)
+
+    assert x.shape == (BATCH_SIZE, T_FULL_INPUT, NUM_CLASSES+1)
     assert seq_indices.shape == (BATCH_SIZE, SEQ_LENGTH)
 
     model.train()
     outputs = model(x)
-    assert outputs.shape == (BATCH_SIZE, SEQ_LENGTH, NUM_CLASSES)
-    loss = loss_func(outputs.transpose(1, 2), seq_indices)
+    assert outputs.shape == (BATCH_SIZE, NUM_CLASSES, SEQ_LENGTH)
+    loss = loss_func(outputs, seq_indices)
 
-    optimizer.zero_grad()
-    loss.backward()
-    optimizer.step()
+    learn_loss += loss
+    # Iterate through sequence lengths
+    if SEQ_LENGTH == seq_length_max:
+        SEQ_LENGTH = seq_length_min
+        learn_loss /= 1 + (seq_length_max - seq_length_min)
+        optimizer.zero_grad()
+        learn_loss.backward()
+        optimizer.step()
+        learn_loss = 0
+    else:
+        SEQ_LENGTH += 1
+    T_SEQUENCE = SEQ_LENGTH * INPUT_STEP_T
+    T_FULL_INPUT = (SEQ_LENGTH+1) * INPUT_STEP_T
+    T_OUTPUT_START = T_SEQUENCE + T_AFTER_GO_DELAY
+
     avg_loss += loss.item()
     if (i+1) % LR_UPDATES == 0:
         avg_loss /= LR_UPDATES
         print('Step [{}/{}], Loss: {:.4f}'
               .format(i + 1, LEARNING_STEPS, avg_loss))
         scheduler.step(avg_loss)
-    optimizer.state.update()
+        if avg_loss < 0.01:
+            break
+        avg_loss = 0
     i += 1
     if i == LEARNING_STEPS:
         break
+
+
+
 
 # "Validation"
 # model.eval()
@@ -212,20 +254,60 @@ for (x, seq_indices) in loader:
 #         print(f"Output sequence: {outputs}")
 
 # Testing model
+# model.eval()
+# with torch.no_grad():
+#     tot = 0
+#     correct = 0
+#     n_tests = 4000
+#     print_step = 100
+#     for i in range(n_tests):
+#         SEQ_LENGTH = torch.randint(seq_length_min, seq_length_max + 1, (1,)).item()
+#         T_SEQUENCE = SEQ_LENGTH * INPUT_STEP_T
+#         T_FULL_INPUT = (SEQ_LENGTH+1) * INPUT_STEP_T
+#         T_OUTPUT_START = T_SEQUENCE + T_AFTER_GO_DELAY
+
+#         (x, seq_indices) = next(iter(dataset))
+#         x = x.unsqueeze(0)  # i.e. 1 batch size
+#         outputs = model(x)
+#         _, predicted = torch.max(outputs.data, 1)
+
+#         tot += SEQ_LENGTH
+#         correct = correct + (predicted == seq_indices).sum().item()
+#         if (i+1) % print_step == 0:
+#             print(f"Test {i+1}:\n  Input sequence: \t{seq_indices.tolist()}")
+#             print(f"  Output sequence: \t{predicted.squeeze().tolist()}")
+
+#     accuracy = 100 * correct / tot
+#     print(f"Test accuracy of the model on {n_tests} sequences: {accuracy} %")
+
+
+# Testing per length:
 model.eval()
 with torch.no_grad():
-    correct = 0
-    n_tests = 4000
-    print_step = 100
-    for i in range(n_tests):
-        (x, seq_indices) = next(iter(dataset))
-        x = x.unsqueeze(0)  # i.e. 1 batch size
-        outputs = model(x).transpose(1, 2)
-        _, predicted = torch.max(outputs.data, 1)
-        correct = correct + (predicted == seq_indices).sum().item()
-        if (i+1) % print_step == 0:
-            print(f"Test {i+1}:\n  Input sequence: {seq_indices.tolist()}")
-            print(f"  Output sequence: {predicted.squeeze().tolist()}")
+    A = []
+    for seq_len in range(seq_length_min-1, seq_length_max + 3):
+        tot = 0
+        correct = 0
+        n_tests = 1000
+        print_step = 500
+        for i in range(n_tests):
+            SEQ_LENGTH = seq_len
+            T_SEQUENCE = SEQ_LENGTH * INPUT_STEP_T
+            T_FULL_INPUT = (SEQ_LENGTH+1) * INPUT_STEP_T
+            T_OUTPUT_START = T_SEQUENCE + T_AFTER_GO_DELAY
 
-    accuracy = 100 * correct / SEQ_LENGTH / n_tests
-    print(f"Test accuracy of the model on {n_tests} sequences: {accuracy} %")
+            (x, seq_indices) = next(iter(dataset))
+            x = x.unsqueeze(0)  # i.e. 1 batch size
+            outputs = model(x)
+            _, predicted = torch.max(outputs.data, 1)
+
+            tot += SEQ_LENGTH
+            correct = correct + (predicted == seq_indices).sum().item()
+            if (i+1) % print_step == 0:
+                print(f"Test {i+1}:\n  Input sequence: \t{seq_indices.tolist()}")
+                print(f"  Output sequence: \t{predicted.squeeze().tolist()}")
+
+        accuracy = 100 * correct / tot
+        A.append(accuracy)
+        print(f"Test accuracy of the model on {n_tests} sequences, length {seq_len}: {accuracy} %")
+    print(A)
