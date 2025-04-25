34e370965fb8ddfdf05c36c143f7893d191dfd14
Hamezii <hameziipost@gmail.com>
2025-04-25 14:39:43 +0100

    Add WIP plot_memory_neuron_state

--- code/004_extended/plotting.py
+++ code/004_extended/plotting.py
@@ -4,6 +4,34 @@ import matplotlib.pyplot as plt
 
 VERBOSE = False
 
+# def plot_memory_neuron_state(model, states, save=None):
+#     # Plot state variables in a 2x2 figure
+#     fig, axes = plt.subplots(2, 2, figsize=(12, 8))
+#     titles = ['Mean h (Cluster)', 'Mean u (Cluster)', 'Mean x (Cluster)', 'h_I']
+
+#     for i, variable in enumerate(state_vars):
+#         ax = axes[i // 2, i % 2]
+#         if variable in per_neuron_vars:
+#             for p in range(input_length):
+#                 ax.plot(state_traces[variable][p], label=f'Cluster {p+1}')
+#         else:  # Scalar variable
+#             ax.plot(state_traces[variable], label=variable)
+#         ax.set_title(titles[i])
+#         ax.set_xlabel('Time (0.1ms steps)')
+#         ax.set_ylabel(variable)
+#         ax.grid(alpha=0.15)
+#         ax.legend()
+
+#     plt.tight_layout()
+#     if save is not None:
+#         plt.savefig(save)
+#         if VERBOSE:
+#             print(f"Saving to {save}")
+#     if show:
+#         plt.show()
+#     plt.close()
+
+
 def plot_impulses(impulses, dt, batch_id, y_label="Input", title=None, show=True, save=None):
     """Plot impulses (time_i, batch_size, channels).
     
@@ -30,6 +58,7 @@ def plot_impulses(impulses, dt, batch_id, y_label="Input", title=None, show=True
 
     plt.grid(alpha=0.15)
     plt.legend()
+    plt.tight_layout()
     if save is not None:
         plt.savefig(save)
         if VERBOSE:
